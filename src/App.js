f4f0d05b2d46229ecaf7f16a3e3c1890d5bc099d
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-16 13:57:00 -0700

    aws config test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THIS IS THE STORE!
         </a>
       </header>
     </div>
