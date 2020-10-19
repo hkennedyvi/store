a0ff07b505e59ead7842ff5c920b509e5ae63bd2
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-19 13:21:31 -0700

    grid footer v1

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './footer.scss';
 
 function Footer() {
-    return (
-      <div className="div-test">FOOTER</div>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <p>THANKS FOR VISITING OUR SITE</p>
+      </div>
+      <div className="footer-content">
+        <i class="fab fa-twitter-square"></i>
+      </div>
+      <div className="footer-content">
+        <i class="fab fa-twitter-square"></i>
+      </div>
+      <div className="footer-content">
+        <i class="fab fa-twitter-square"></i>
+      </div>
+      <div className="footer-content">
+        <i class="fab fa-twitter-square"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
