d7ff50ec0d2315c6ec04c0bd9be25a7f56310cff
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-19 13:50:05 -0700

    main content grid v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
     <NavBar />
+    <Main />  
     <Footer />
     </div>
   );
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './main.scss';
+
+function Main() {
+
+    return (
+        <div className="main-container">
+            <div className="main-content wide-content">1</div>
+            <div className="main-content tall-content">2</div>
+            <div className="main-content">3</div>
+            <div className="main-content">4</div>
+            <div className="main-content">5</div>
+            <div className="main-content">6</div>
+            <div className="main-content">1</div>
+            <div className="main-content">2</div>
+            <div className="main-content">3</div>
+            <div className="main-content">4</div>
+            <div className="main-content">5</div>
+            <div className="main-content">6</div>
+        </div>
+    );
+}
+
+export default Main;
