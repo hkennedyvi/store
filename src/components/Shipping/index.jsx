a38f81f9c1f555d59bc07d22f5d5df8ed58fa14f
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-25 01:15:22 -0700

    shipping cards coloring and bordering

--- src/components/Shipping/index.jsx
+++ src/components/Shipping/index.jsx
@@ -9,14 +9,24 @@ function Shipping() {
                 <div className="shipping-card-inner">
                     <div className="shipping-card-front">
                         <h1 className="shipping-card-header">
-                            SHIPPING CARD
-                    </h1>
+                            1.
+                        </h1>
                         <div>
-                            <i className="fas fa-shipping-fast"></i>
+                            <i className="fas fa-box"></i>
                         </div>
                     </div>
                     <div className="shipping-card-back">
-                        THIS IS THE BACK
+                        <p className="shipping-card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore
+                            eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -24,14 +34,24 @@ function Shipping() {
                 <div className="shipping-card-inner">
                     <div className="shipping-card-front">
                         <h1 className="shipping-card-header">
-                            SHIPPING CARD
-                    </h1>
+                            2.
+                        </h1>
                         <div>
                             <i className="fas fa-shipping-fast"></i>
                         </div>
                     </div>
                     <div className="shipping-card-back">
-                        THIS IS THE BACK
+                        <p className="shipping-card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore
+                            eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -39,14 +59,24 @@ function Shipping() {
                 <div className="shipping-card-inner">
                     <div className="shipping-card-front">
                         <h1 className="shipping-card-header">
-                            SHIPPING CARD
-                    </h1>
+                            3.
+                        </h1>
                         <div>
-                            <i className="fas fa-shipping-fast"></i>
+                            <i className="fas fa-box-open"></i>
                         </div>
                     </div>
                     <div className="shipping-card-back">
-                        THIS IS THE BACK
+                        <p className="shipping-card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore
+                            eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </p>
                     </div>
                 </div>
             </div>
