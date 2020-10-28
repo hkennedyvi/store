c55f14b5e2c24d52cd63a6fa8b8b42cbe9d31f3d
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-28 14:21:48 -0700

    card colors/layout change

--- src/components/Shipping/index.jsx
+++ src/components/Shipping/index.jsx
@@ -8,24 +8,22 @@ function Shipping() {
             <div className="shipping-card">
                 <div className="shipping-card-inner">
                     <div className="shipping-card-front">
-                        <h1 className="shipping-card-header">
-                            1.
-                        </h1>
                         <div>
                             <i className="fas fa-box"></i>
                         </div>
+                        <h1 className="shipping-card-header">
+                            1
+                        </h1>
                     </div>
                     <div className="shipping-card-back">
+                        <h2 className="shipping-card-secondary-header">
+                            PACK
+                        </h2>
                         <p className="shipping-card-text">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit, sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
+                            nostrud exercitation ullamco laboris nisi ut aliquip.
                         </p>
                     </div>
                 </div>
@@ -33,24 +31,22 @@ function Shipping() {
             <div className="shipping-card">
                 <div className="shipping-card-inner">
                     <div className="shipping-card-front">
-                        <h1 className="shipping-card-header">
-                            2.
-                        </h1>
                         <div>
                             <i className="fas fa-shipping-fast"></i>
                         </div>
+                        <h1 className="shipping-card-header">
+                            2
+                        </h1>
                     </div>
                     <div className="shipping-card-back">
+                        <h2 className="shipping-card-secondary-header">
+                            SHIP
+                        </h2>
                         <p className="shipping-card-text">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit, sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
+                            nostrud exercitation ullamco laboris nisi ut aliquip.
                         </p>
                     </div>
                 </div>
@@ -58,29 +54,27 @@ function Shipping() {
             <div className="shipping-card">
                 <div className="shipping-card-inner">
                     <div className="shipping-card-front">
-                        <h1 className="shipping-card-header">
-                            3.
-                        </h1>
                         <div>
                             <i className="fas fa-box-open"></i>
                         </div>
+                        <h1 className="shipping-card-header">
+                            3
+                        </h1>
                     </div>
                     <div className="shipping-card-back">
+                        <h2 className="shipping-card-secondary-header">
+                            ENJOY
+                        </h2>
                         <p className="shipping-card-text">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit, sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
+                            nostrud exercitation ullamco laboris nisi ut aliquip.
                         </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
