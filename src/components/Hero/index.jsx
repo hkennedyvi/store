efb98fd635b799861d058152e15ba22a15cac092
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-29 13:42:13 -0700

    learn more button routed

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,8 +5,10 @@ function Hero() {
 
     return (
         <div className="hero">
-            <h1 className="header">BIG THINGS ARE COMING</h1>
+            <h1 className="header">BIG THINGS ARE HERE</h1>
+            <a className="nav-anchor" href="/about">
             <button className="button">LEARN MORE</button>
+            </a>
         </div>
     );
 }
