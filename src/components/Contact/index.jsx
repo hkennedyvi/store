1b2d5cf69fb6e03127126d510b91ef0565aa1a4c
Howard Kennedy <hkennedyvi@gmail.com>
2020-10-30 18:41:11 -0700

    contact form v2

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,28 +2,33 @@ import React from 'react';
 import './contact.scss';
 
 function Contact() {
+
+    function sendMessage(e) {
+        e.preventDefault();
+    }
     return (
         <div className="contact">
-            {/* <h1 className="contact-header">COMING SOON</h1> */}
             <form className="contact-form" action="">
-                <h3 className="contact-header">CONTACT</h3>
-                <label htmlFor="">
+                <h3 className="contact-header">
+                    HOLLER AT US
+                </h3>
+                <label className="contact-label" htmlFor="">
                     First Name:
                     <input className="contact-input" type="text" />
                 </label>
-                <label htmlFor="">
+                <label className="contact-label" htmlFor="">
                     Last Name:
                     <input className="contact-input" type="text" />
                 </label>
-                <label htmlFor="">
+                <label className="contact-label" htmlFor="">
                     Email:
                     <input className="contact-input" type="text" />
                 </label>
-                <label htmlFor="">
+                <label className="contact-label" htmlFor="">
                     Message:
-                <textarea className="contact-textarea" name="" id="" cols="100" rows="15" />
                 </label>
-                <input className="contact-submit" type="submit" value="Send"/>
+                <textarea className="contact-textarea" name="" id="" cols="100" rows="15" />
+                <input onClick={sendMessage} className="contact-submit" type="submit" value="Send" />
             </form>
         </div>
     )
